7bb24c1e100219c964e8ab57b05f93ff40b0037b
EC2 Default User <ec2-user@ip-172-31-44-111.us-east-2.compute.internal>
2018-09-19 13:08:36 +0000

    Controllers and Views

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 module.exports.controller = (app) => { 
     // get users page 
     app.get('/users', (req, res) => { 
-        res.render('index', { title: 'Users' });
+        res.render('users', { 
+            title: 'Users',
+            description: 'Yet another Javascript full stack technology',});
         }) 
 }
